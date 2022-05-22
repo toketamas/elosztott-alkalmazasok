c3bbd5369787d557ff0f6bd53d8f432345fe8468
toketamas <toketamas@gmail.com>
2022-05-22 20:51:18 +0200

    Cegjegyzek classhoz  mentes betoltes hozzaadva

--- src/cegek/Model/Cegjegyzek.java
+++ src/cegek/Model/Cegjegyzek.java
@@ -4,6 +4,11 @@
  */
 package cegek.Model;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -14,7 +19,7 @@ import java.io.Serializable;
  *
  * @author ttoke
  */
-public class Cegjegyzek implements ICegjegyzek, Serializable{
+public class Cegjegyzek implements ICegjegyzek, Serializable {
 
     private ArrayList<Ceg> lista;
 
@@ -44,13 +49,34 @@ public class Cegjegyzek implements ICegjegyzek, Serializable{
 
     @Override
     public void cegjegyzekMentes() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            FileOutputStream file = new FileOutputStream("cegjegyzek.ser");
+            ObjectOutputStream out = new ObjectOutputStream(file);
+            out.writeObject(this);
+            out.close();
+            file.close();
+            System.out.println("A cégjegyzék mentése megtörtént!");
+        } catch (IOException ex) {
+            System.out.println("A cégjegyzék mentése sikertelen.");
+        }
     }
 
     @Override
-    public void cegjegyzekBetoltes() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Cegjegyzek cegjegyzekBetoltes() {
+        try {
+            FileInputStream file = new FileInputStream("cegjegyzek.ser");
+            ObjectInputStream in = new ObjectInputStream(file);
+            Cegjegyzek cegjegyzek = (Cegjegyzek) in.readObject();
+
+            in.close();
+            file.close();
+            System.out.println("Cégjegyzék beolvasva");
+            return cegjegyzek;
+        } catch (IOException ex) {
+            System.out.println("Cégjegyzék betöltése sikertelen");
+            return null;
+        }
+
     }
 
-   
 }
